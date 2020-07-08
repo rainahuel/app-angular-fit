bb1e11bbb1b7be173354e0153ee8e1c2c8e77fc2
aarainahuel <aarainahuel@indracompany.com>
2020-07-07 21:28:07 -0400

    cambios en environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,21 @@
 export const environment = {
-  production: true
+  production: true,
+  urlBase: 'http://localhost:5000/',
+  api: {
+    usuario: {
+      login: 'login',
+      crearUsuario: 'usuarios'
+    },
+    rutinas: {
+      obtenerRutinas: 'rutinas',
+      actializaRutina: 'rutinas',
+      crearRutina: 'rutinas',
+      eliminarRutina: 'rutinas',
+    },
+    perfiles: {
+      obtenerPerfil: 'perfiles/',
+      agregarPerfil: 'perfiles?token=',
+      quitarPerfil: 'perfiles?token=',
+    }
+  }
 };
